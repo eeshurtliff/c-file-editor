c574e12d9e930935524b46b37708e2e6cc81223b
eeshurtliff <eeshurtliff@gmail.com>
2024-11-12 13:29:34 -0700

    updated main

--- file-editor-main.cpp
+++ file-editor-main.cpp
@@ -16,11 +16,19 @@ int main(){
     bool stillWorking = true;
     while(stillWorking){
         menu();
+        int choice = getChoice();
+        if (choice == 1){
+            file.updateLines();
+            file.display();
 
+        }else if (choice == 2){
+            file.edit();
+        }else{
+            stillWorking = false;
+        }
 
     }
-
-
+    cout << "Thanks for using the file editor!" << endl;
 
     return 0;
 
@@ -30,5 +38,36 @@ int menu(){
 
     cout << "1. View the text in the file" << endl;
     cout << "2. Change a line in the file.  " << endl;
+    cout << "3. Exit. " << endl;
+
+}
+
+
+int getChoice(){
+    int choice;
+
+    bool badChoice = true;
+
+
+    while (badChoice) {
+                
+        int newValue;
+        if ( !(cin >> choice) ){
+                cin.clear();
+                cin.ignore();
+                cout << "Incorrect entry. Try again: ";
+                // cin >> newValue;
+                // chosenLine = newValue;
+        } else if (choice > 0 && choice <= 3) {
+                cout << choice << " done!" << endl;
+                badChoice = false;  
+        } else {
+                cout << "Incorrect entry. Try again. ";
+                // cin >> newValue;
+                // chosenLine = newValue;  
+        }
+    }
+
+    return choice;
 
 }
